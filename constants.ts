9b9b1c7cdd15384d57a64191bdeccf88a9c0b131
VlogLife786 <vloglife643@gmail.com>
2025-09-28 09:14:46 +0000

    First commit

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export enum PageNames {
+    WAN_AI = "Wan AI",
+    YOPMAIL = "YOPmail",
+    INBOX = "Inbox"
+}
+
+export class Flags {
+    static readonly BROWSER_LOCAL: boolean = false;
+}
---
+++ index.ts
@@ -0,0 +1,158 @@
+import puppeteer, { Browser, ElementHandle, Page } from "puppeteer";   // If "type": "module" in package.json
+import { Flags, PageNames } from "./constants.js";
+import Chromium from "@sparticuz/chromium";
+
+const browser: Browser = Flags.BROWSER_LOCAL ?
+    await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized']
+    })
+    :
+    await puppeteer.launch({
+        args: Chromium.args,
+        defaultViewport: null,         // optional: to see full page
+        executablePath: await Chromium.executablePath(),
+        headless: false,
+    }); // headless:false shows the browser
+
+(async () => {
+
+    try {
+        let tempMail = "siddhesh123@yopmail.com";
+        await wanAIRegistration(tempMail, "Siddhesh Gathibandhe", "Siddhesh18");
+        await yopmail(tempMail)
+        let otp: string = await getWanOTPFromYopmail();
+        await focusOnPage(PageNames.WAN_AI);
+        let wanPage: Page = await getPageFromOpenedByPageName(PageNames.WAN_AI);
+
+        wanPage.type('[placeholder="Verification code"]', otp);
+        await sleep(3000);
+        await wanPage.click('[type="submit"]');
+        await wanPage.waitForNavigation();
+        await wanPage.waitForSelector('.ant-modal-close-x', { visible: true });
+        await wanPage.click('.ant-modal-close-x');
+        let checkInButton = await wanPage.waitForSelector('.sc-jytpVa button', { visible: true }) as ElementHandle<HTMLButtonElement>;
+        await checkInButton.evaluate(el => el.scrollIntoView());
+        await checkInButton.click();
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+
+
+
+    // await page.type('[data-test-id="login-form-box-password"]', 'Siddhesh18');
+
+
+    // Take a screenshot
+    //   await page.screenshot({ path: "example.png" });
+
+    // Close browser
+    // await browser.close();
+})();
+
+async function yopmail(emailId: string) {
+    const yopmail: Page = await browser.newPage();
+    await yopmail.goto("https://yopmail.com/", {
+        waitUntil: "load"
+    })
+    const yopmailTitle = await yopmail.title();
+    console.log("Page title: ", yopmailTitle);
+    await yopmail.waitForSelector('#ycptcpt');
+    await yopmail.type('[placeholder="Enter your inbox here"]', emailId);
+    await Promise.all([
+        yopmail.click('[title="Check Inbox @yopmail.com"]'),
+        yopmail.waitForNavigation()
+    ]);
+    await sleep(5000);
+    await yopmail.click("#refresh");
+    await sleep(3000);
+}
+
+async function generateTempMail() {
+    const tempMail: Page = await browser.newPage();
+    await tempMail.goto("https://tempmailo.com/", {
+        waitUntil: "load"
+    })
+    const tempMailTitle = await tempMail.title();
+    console.log("Page title: ", tempMailTitle);
+    await tempMail.waitForFunction(() => {
+        let emailId: HTMLInputElement = document.querySelector('#i-email') as HTMLInputElement;
+        return (emailId && emailId.value && emailId.value.trim() != '');
+    });
+    return await tempMail.$eval('#i-email', el => (el as HTMLInputElement).value.trim());
+}
+
+async function wanAIRegistration(emailId: string, name: string, password: string) {
+    // Launch browser
+    const page = await browser.newPage();
+    await page.setViewport({ width: 0, height: 0 });
+    // Go to a website
+    await page.goto("https://create.wan.video/generate/video/image-to-video?model=wan2.5", {
+        waitUntil: "load"
+    });
+
+    // Extract the title
+    const title = await page.title();
+    console.log("Page title: ", title);
+
+    await page.waitForSelector('.sc-cokDIm');
+    await page.click(".sc-cokDIm");
+
+    await page.waitForSelector('[placeholder="Confirm password"]', { visible: true });
+    await page.type('[placeholder="Email address"]', emailId);
+    await page.type('[placeholder="Password (8-20 characters)"]', password);
+    await page.type('[placeholder="Confirm password"]', password);
+    await page.type('[placeholder="Your name"]', name);
+
+    await Promise.all([
+        page.click('[type="submit"]'),
+        sleep(3000)
+    ]);
+    // await sleep(3000);
+}
+
+async function focusOnPage(pageName: string): Promise<void> {
+    let pages: Page[] = await browser.pages();
+    for (const page of pages) {
+        let currentPageName = await page.title();
+        if (currentPageName.includes(pageName)) {
+            page.bringToFront();
+        }
+    }
+}
+
+
+async function getPageFromOpenedByPageName(pageName: string): Promise<Page> {
+    let pages: Page[] = await browser.pages();
+    for (const page of pages) {
+        let currentPageName = await page.title();
+        if (currentPageName.includes(pageName)) {
+            return page;
+        }
+    }
+    throw new Error("Page not found: " + pageName);
+}
+
+async function getWanOTPFromYopmail(): Promise<string> {
+    let yopmail: Page = await getPageFromOpenedByPageName(PageNames.INBOX);
+    const iframeHandle = await yopmail.$('iframe#ifmail'); // Replace with your iframe selector
+    if (!iframeHandle) throw new Error("Iframe not found");
+
+    // 2. Get the content frame
+    const frame = await iframeHandle.contentFrame();
+    if (!frame) throw new Error("Failed to get iframe content");
+
+    // 3. Wait for the div inside the iframe to appear
+    await frame.waitForSelector('#mail');
+
+    let divText: string | null = await frame.$eval('#mail', el => el.textContent?.trim() ?? null);
+    let match = divText?.match(/\b\d{6}\b/); // match exactly 6 digits
+    let otp = match ? match[0] : ""
+
+    return otp;
+}
+
+async function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
