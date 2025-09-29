859473d68040f79f903b1104c802bdef373713fc
VlogLife786 <vloglife643@gmail.com>
2025-09-29 14:08:48 +0000

    test

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import { startProcessOfAccountCreation } from "./utility.js";
-export default async function main() {
+export default async function main({ req, res }) {
     await startProcessOfAccountCreation();
+    return res.json({ message: "Hello from Appwrite Function 🚀" });
 }
 // nodeCron.schedule("* * * * *", () => {
 //     main();
--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import nodeCron from "node-cron";
 import { startProcessOfAccountCreation } from "./utility.js";
 
 
-export default async function main() {
+export default async function main({ req, res }: any) {
     await startProcessOfAccountCreation();
+     return res.json({ message: "Hello from Appwrite Function 🚀" });
 }
 
 // nodeCron.schedule("* * * * *", () => {
