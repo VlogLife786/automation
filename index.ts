1cb339053d45f9e764901b0db6836b8ec5e0c63e
VlogLife786 <vloglife643@gmail.com>
2025-10-07 10:18:24 +0000

    index.js uncomment cron job

--- index.ts
+++ index.ts
@@ -9,26 +9,26 @@ import { registerToDremaniaAi } from "./dremaniaRegister.js";
 configDotenv();
 const schedularTime: string = process.env.SCHEDULAR_TIME || Configs.SCHEDULAR_CONFIG;
 
-(async function main() {
-    await registerToDremaniaAi();
-})();
+// (async function main() {
+//     await registerToDremaniaAi();
+// })();
 
-// nodeCron.schedule(schedularTime, async () => {
-//     switch (process.env.ACTION) {
-//         case Constant.ACTION_REGISTER:
-//             await startProcessOfAccountCreation();
-//             break;
+nodeCron.schedule(schedularTime, async () => {
+    switch (process.env.ACTION) {
+        case Constant.ACTION_REGISTER:
+            await startProcessOfAccountCreation();
+            break;
 
-//         case Constant.ACTION_LOGIN:
-//             await startProcessOfAccountLogin();
-//             break;
+        case Constant.ACTION_LOGIN:
+            await startProcessOfAccountLogin();
+            break;
 
-//         case Constant.DREMANIA_REGISTER:
-//             await registerToDremaniaAi();
-//             break;
+        case Constant.DREMANIA_REGISTER:
+            await registerToDremaniaAi();
+            break;
 
-//         default:
-//             console.log("Invalid action");
-//             break;
-//     }
-// })
+        default:
+            console.log("Invalid action");
+            break;
+    }
+})
