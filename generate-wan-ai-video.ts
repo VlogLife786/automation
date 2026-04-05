49ed71a366d7957a82a88a3583b90ca41a93f925
vloglife643 <vloglife643@gmail.com>
2026-04-05 05:59:38 +0000

    Added logs

--- generate-wan-ai-video.ts
+++ generate-wan-ai-video.ts
@@ -6,7 +6,7 @@ import { postRestResponse } from "./restTemplate.js";
 var browser: Browser;
 
 export async function GenerateWANAiVideos(prompt: string, loginEmail: string, loginPassword: string, emailToSendVideo: string, webhookUrl: string, rowNumber: number = 0) {
-
+    let logsPoints: string[] = [];
     console.log("Received the request of execution...");
 
     browser = await openNewBrowser(Flags.BROWSER_SERVER);
@@ -16,6 +16,7 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
         //Navigate to wan ai
         await page.goto("https://create.wan.video/generate", { waitUntil: "load" });
         await sleep(3000);
+        logsPoints.push("Navigated to WAN AI Site.");
 
         //Navigate to login page
         // await clickBySelector(page, "[class*=HeaderContainer] [class*=RightContent] button");
@@ -24,12 +25,17 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
         //Login with user credentials
         await page.type('[data-test-id="login-form-box-address"]', loginEmail, { delay: 120 });
         await sleep(1000);
+        logsPoints.push("Email ID is entered: " + loginEmail);
+
         await page.type('[data-test-id="login-form-box-password"]', loginPassword, { delay: 120 });
         await sleep(1000);
+        logsPoints.push("Password is eneterd.");
+
         await Promise.all([
             page.waitForNavigation({ waitUntil: "networkidle2" }),
             page.click('[data-test-id="login-form-button-submit"]')
         ]);
+        logsPoints.push("Clicked on login button.");
 
         // //Reload page once
         // await page.reload({ waitUntil: "networkidle2" });
@@ -37,25 +43,33 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
         //Navigate to generated videos
         await page.goto("https://create.wan.video/generate", { waitUntil: "load" });
         await sleep(5000);
+        logsPoints.push("Navigated to generate video section.");
 
         //Claim credits
         await page.reload({ waitUntil: "networkidle2" }); //For faaltu popup
         await sleep(3000);
+        logsPoints.push("Page reloaded to avoid additional pop-up ads..");
 
         await page.click('[data-test-id="header-popover-button-credit"] [class*=CreditText]');
         await sleep(500);
         await page.click('[class*=BtnContainer] button:last-of-type');
         await sleep(2000);
+        logsPoints.push("Credits are claimed.");
+
         // await captureScreenShot(page, "credits")
         await page.reload({ waitUntil: "networkidle2" });
         await sleep(5000);
+        logsPoints.push("Page reloaded to avoid additional pop-up ads.");
 
 
         //Enter the prompt
         await page.click('[data-slate-node="element"]');
         await sleep(1000);
+        logsPoints.push("Clicked on textbox where prompt will be written.");
+
         await page.type('[data-slate-node="element"]', prompt);
         console.log("Prompt added successfully.");
+        logsPoints.push("Prompt added to textbox.");
 
 
         //Check existing created videos
@@ -71,10 +85,12 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
         await sleep(1000);
         await page.click('[class*=SettingWrapper] div:last-of-type');
         await sleep(500);
+        logsPoints.push("Changed the resolution according to mobile devices");
 
         //Click on generate video
         await page.click('[data-test-id="creation-form-button-submit"]');
         await sleep(5000);
+        logsPoints.push("Generate button is clicked.");
 
         //Check video generation is added in queue
         await page.reload({ waitUntil: "networkidle2" });
@@ -86,15 +102,19 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
         //Check generation process is started
         if (text.includes("Generating")) {
             console.log("Video generation is on queue");
+            logsPoints.push("Check that video generation is started and wait until video generation done.");
 
             //Wait until generation process not ends
             outerLoop: for (let index = 0; index < 100; index++) {
                 let newlyCreatedVideoCount = await page.$$eval('[data-test-id="dragable-content"]', els => els.length);
+                logsPoints.push("Check that video is generated or not.");
 
                 //get the video url of latest video
                 if (newlyCreatedVideoCount > alreadyCreatedVideoCount) {
                     await page.reload({ waitUntil: "load" });
                     await sleep(5000);
+                    logsPoints.push("Video is generated, Now grabbing the video url.");
+
                     for (let index = 0; index < 100; index++) {
                         const src = await page.$eval(
                             '[data-test-id="dragable-content"] video',
@@ -104,6 +124,7 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
                         if (src && src != "") {
                             videoUrl = src;
                             console.log(videoUrl);
+                            logsPoints.push("Video URL is also generated, Now sending it to the requested user.");
 
                             //Send video to user
                             await postRestResponse(webhookUrl, {
@@ -125,29 +146,39 @@ export async function GenerateWANAiVideos(prompt: string, loginEmail: string, lo
 
         //Video is blank throw exception
         if (videoUrl == "") {
+            logsPoints.push("Video URL is not generated or found.");
             throw new Error(`Video is not generated for user: ${loginEmail}`)
         }
 
         //Logout current user
         await page.click('[data-test-id="header-popover-button-user"]');
         await sleep(500);
+        logsPoints.push("Clicked on profile icon.");
+
         await page.click('[class*=PopoverContent] > div:last-of-type');
         await sleep(1000);
+        logsPoints.push("Clicked on logout option.");
 
         const buttons = await page.$$('button');
 
         for (const button of buttons) {
-            const text = await page.evaluate(el => el.textContent, button);
+            const text: any = await page.evaluate(el => el.textContent, button);
             if (text.trim() === 'Confirm') {
                 await button.click();
                 break;
             }
         }
+        logsPoints.push("clicked on confirm logout option.");
 
         await page.reload({ waitUntil: "networkidle2" });
 
-    } catch (error) {
-        console.log(error);
+    } catch (error: any) {
+        try {
+
+        } catch {
+            console.log("Internal error while sending mail of image");
+        }
+        console.log(error?.message);
         console.log("Execution completed with errors.");
         throw error;
     } finally {
